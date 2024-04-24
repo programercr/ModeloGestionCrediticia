dabc8cffe5b7b11dd78c75605e366ebb1845c32c
programercr@gmail.com <programercr@gmail.com>
2024-04-23 22:25:22 -0600

    actualizando nombre de archivos

--- calculo_endeudamiento.py
+++ calculo_endeudamiento.py
@@ -1,4 +1,4 @@
-import ingresos 
+import informe_ingresos 
 import deudas_clientes
 
 #nivel_endeudamiento (total_deudas)/(ingreso_promedio_mensual)
@@ -6,7 +6,7 @@ import deudas_clientes
 salario1 = float(input("Ingresar Salario 1: "))
 salario2 = float(input("Ingresar Salario 2: "))
 salario3 = float(input("Ingresar Salario 3: "))
-salario_promedio= ingresos.ingreso_promedio(salario1, salario2, salario3)
+salario_promedio= informe_ingresos.ingreso_promedio(salario1, salario2, salario3)
 deudas=deudas_clientes.ingresar_deudas_crediticias()
 nivel_endeudamiento=deudas/salario_promedio
 print(f'calculo de endeudamiento: {round(nivel_endeudamiento,2)}')
