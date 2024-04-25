7fd8327763be29090b1ccd92308e516f2b8fd9d4
¨Johnjim0723¨ <johnjim0723@icloud.com>
2024-04-24 22:34:34 -0600

    Mejora en codigo de ingreso_datos_clientes

--- ingreso_datos_clientes.py
+++ ingreso_datos_clientes.py
@@ -10,18 +10,48 @@ class Usuario:
         self.telefono = ""
 
     def ingresar_informacion(self):
-        self.documento_id = input("Ingrese el Documento de Identificación: ")
+        while True:
+            self.documento_id = input("Ingrese el Documento de Identificación: ")
+            if not self.documento_id.isdigit() or len(self.documento_id) != 9:
+                print("Por favor, ingrese un documento de identificación válido con 9 dígitos.")
+            else:
+                break
+
         self.nombre = input("Ingrese el Nombre: ")
+        while not self.nombre.isalpha():
+            print("El nombre solo puede contener letras.")
+            self.nombre = input("Ingrese el Nombre: ")
+
         self.apellidos = input("Ingrese sus Apellidos: ")
+        while not self.apellidos.replace(" ", "").isalnum():
+            print("Los apellidos solo pueden contener caracteres alfanuméricos y espacios.")
+            self.apellidos = input("Ingrese sus Apellidos: ")
+
         self.fecha_nacimiento = input("Ingrese la Fecha de Nacimiento (YYYY-MM-DD): ")
+
         self.estado_civil = input("Ingrese el Estado Civil: ")
-        self.provincia = input("Ingresar Provincia: ")
+
+        provincias_validas = ["cartago", "limon", "san jose", "heredia", "guanacaste", "alajuela", "puntarenas"]
+        while True:
+            self.provincia = input("Ingresar Provincia: ")
+            if self.provincia not in provincias_validas:
+                print("Provincia inválida. Por favor, ingrese una provincia válida.")
+            else:
+                break
+
         self.correo_electronico = input("Ingrese el Correo Electrónico: ")
-        self.telefono = input("Ingrese Teléfono: ")
+
+        while True:
+            self.telefono = input("Ingrese Teléfono: ")
+            if not self.telefono.isdigit() or len(self.telefono) != 8:
+                print("Por favor, ingrese un número de teléfono válido con 8 dígitos.")
+            else:
+                break
 
     def obtener_informacion(self):
         return (self.documento_id, self.nombre, self.apellidos, self.fecha_nacimiento,
                 self.estado_civil, self.provincia, self.correo_electronico, self.telefono)
+
 
 
 class Asalariado(Usuario):
@@ -101,4 +131,3 @@ if __name__ == "__main__":
     pensionado = Pensionado()
     pensionado.ingresar_informacion()
     print(pensionado.obtener_informacion())
-    
