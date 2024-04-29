7b3a400aaf398c92f63246b37fdbe2490530617d
programercr@gmail.com <programercr@gmail.com>
2024-04-28 22:03:07 -0600

    ajustanto tipo cliente

--- tipo_cliente.py
+++ tipo_cliente.py
@@ -25,7 +25,7 @@ def tipo_empleo():
         base_de_datos.guardar_usuario(asalariado)  # Guarda la información del usuario en la base de datos
         analizador = AnalizadorDeUsuarios("usuarios.csv")
         analizador.agregar_cuota()
-        info_usuario= analizador.productos(asalariado.documento_id)
+        info_usuario, info_productos= analizador.productos(asalariado.documento_id)
         producto_seleccionado = analizador.elegir_producto()
         analizador.usuario_producto(info_usuario, producto_seleccionado)
 
@@ -36,7 +36,7 @@ def tipo_empleo():
         base_de_datos.guardar_usuario(independiente)  # Guarda la información del usuario en la base de datos
         analizador = AnalizadorDeUsuarios("usuarios.csv")
         analizador.agregar_cuota()
-        info_usuario= analizador.productos(independiente.documento_id)
+        info_usuario, info_productos= analizador.productos(independiente.documento_id)
         producto_seleccionado = analizador.elegir_producto()
         analizador.usuario_producto(info_usuario, producto_seleccionado)
 
@@ -47,7 +47,7 @@ def tipo_empleo():
         base_de_datos.guardar_usuario(pensionado)  # Guarda la información del usuario en la base de datos
         analizador = AnalizadorDeUsuarios("usuarios.csv")
         analizador.agregar_cuota()
-        info_usuario= analizador.productos(pensionado.documento_id)
+        info_usuario, info_productos= analizador.productos(pensionado.documento_id)
         producto_seleccionado = analizador.elegir_producto()
         analizador.usuario_producto(info_usuario, producto_seleccionado)
         
